f477fd4c77d642a9b153d38aa265d83e80ea7290
Hugo Cariaga <hugocariagacastillo@gmail.com>
2022-06-21 19:26:21 +0100

    Creacion de cards en principalPage

--- jardin_cliente/lib/pages/principal_page.dart
+++ jardin_cliente/lib/pages/principal_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jardin_cliente/widgets/card.dart';
 
 class PrincipalPage extends StatefulWidget {
   PrincipalPage({Key? key}) : super(key: key);
@@ -7,15 +8,24 @@ class PrincipalPage extends StatefulWidget {
   State<PrincipalPage> createState() => _PrincipalPageState();
 }
 
+
 class _PrincipalPageState extends State<PrincipalPage> {
+  final verdeClaro = Color(0xFF89DA59);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Jardin Infantil'),
         leading: Icon(Icons.child_care),
+        backgroundColor: verdeClaro,
       ),
-      body: Center(child: Text('Jardin Payasito', style: TextStyle(fontSize: 30),),)
+      body: Column(
+        children: [
+          CardWidgets(texto: 'Play Group'),
+          CardWidgets(texto: 'Medio Menor'),
+          CardWidgets(texto: 'Medio Mayor'),
+        ],
+      )
     );
   }
 }
---
+++ jardin_cliente/lib/widgets/card.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class CardWidgets extends StatelessWidget {
+  final String texto;
+  final String imagen;
+
+  const CardWidgets({
+    this.texto = '',
+    this.imagen ='',
+
+    Key? key
+    }) : super(key: key);
+  @override
+
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      margin: EdgeInsets.all(15),
+      elevation: 10,
+
+      // Dentro de esta propiedad usamos ClipRRect
+      child: ClipRRect(
+
+        // Los bordes del contenido del card se cortan usando BorderRadius
+        borderRadius: BorderRadius.circular(30),
+
+        // EL widget hijo que será recortado segun la propiedad anterior
+        child: Column(
+          children: <Widget>[
+
+            // Usamos el widget Image para mostrar una imagen
+            Image(
+              height: 200,
+              width: double.infinity,
+              // Como queremos traer una imagen desde un url usamos NetworkImage
+              image: NetworkImage(
+                //Cambiar por 'imagen' el link
+                  'https://www.yourtrainingedge.com/wp-content/uploads/2019/05/background-calm-clouds-747964.jpg'),
+            ),
+
+            // Usamos Container para el contenedor de la descripción
+            Container(
+              padding: EdgeInsets.all(10),
+              child: Text(texto),
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
