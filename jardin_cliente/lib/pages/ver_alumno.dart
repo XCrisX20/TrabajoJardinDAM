377d3418be1e55f6fc931204f5ac8b9ccea5b4ea
Emanuel Venegas <emanuelvperez2000@gmail.com>
2022-06-25 18:24:13 -0400

    ver alumno funcional

--- jardin_cliente/lib/pages/ver_alumno.dart
+++ jardin_cliente/lib/pages/ver_alumno.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:jardin_cliente/provider/alumnos_provider.dart';
 
 class Ver_Alumnos extends StatefulWidget {
   final int codigo;
   final String rut;
+
   const Ver_Alumnos({this.codigo = 0, this.rut = '', Key? key})
       : super(key: key);
 
@@ -10,16 +12,46 @@ class Ver_Alumnos extends StatefulWidget {
   State<Ver_Alumnos> createState() => _Ver_AlumnosState();
 }
 
+String nombre = '';
+
 class _Ver_AlumnosState extends State<Ver_Alumnos> {
   final verdeClaro = Color(0xFF89DA59);
   @override
+  void initState() {
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: verdeClaro,
         title: Text("Informacion del alumno"),
       ),
-      body: Text(widget.rut),
+      body: FutureBuilder(
+        future: AlumnosProvider().getAlumno(widget.rut),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          var Data = snapshot.data;
+          return Container(
+              child: Column(
+            children: [
+              Row(
+                children: [Text(Data['rut'])],
+              ),
+              Row(
+                children: [Text(Data['nombre'])],
+              ),
+              Row(
+                children: [Text(Data['fechaNacimiento'].toString())],
+              ),
+            ],
+          ));
+        },
+      ),
     );
   }
 }
