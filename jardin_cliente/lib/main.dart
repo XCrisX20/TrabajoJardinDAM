ac4e7d01071e0a2029e58c54424b18a97dd9c931
Emanuel Venegas <emanuelvperez2000@gmail.com>
2022-06-24 22:09:45 -0400

    FechaArreglada

--- jardin_cliente/lib/main.dart
+++ jardin_cliente/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:jardin_cliente/pages/agregar_alumno_form.dart';
 import 'package:jardin_cliente/pages/principal_page.dart';
 
@@ -11,16 +12,22 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
- 
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      supportedLocales: [
+        const Locale('es', 'ES'),
+      ],
+      locale: Locale('es'),
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
       home: PrincipalPage(),
-      
     );
   }
 }
