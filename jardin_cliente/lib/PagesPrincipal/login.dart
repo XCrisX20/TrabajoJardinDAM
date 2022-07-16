06a793b8244322f2a47e033e720b645687a51f07
Emanuel Venegas <emanuelvperez2000@gmail.com>
2022-07-15 23:22:14 -0400

    Revert "Google iniciar Sesion"
    This reverts commit 7018eb0fa786b7e47cd48cdea09c4e3aa7df5623.

--- jardin_cliente/lib/PagesPrincipal/login.dart
+++ jardin_cliente/lib/PagesPrincipal/login.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:jardin_cliente/pages/principal_page.dart';
-import 'package:jardin_cliente/provider/GoogleService.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class IniciarSesion extends StatefulWidget {
   const IniciarSesion({Key? key}) : super(key: key);
@@ -20,7 +17,7 @@ class _IniciarSesionState extends State<IniciarSesion> {
     GoogleSignInAccount? user = _googleSignIn.currentUser;
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hola Usuario'),
+        title: Text('Hola Usuario ' + (user == null ? 'entrada' : 'salida')),
         leading: Icon(MdiIcons.humanHandsup),
       ),
       body: Center(
@@ -30,14 +27,18 @@ class _IniciarSesionState extends State<IniciarSesion> {
                 onPressed: user != null
                     ? null
                     : () async {
-                        GoogleServicio().signInWithGoogl();
-                        MaterialPageRoute route = MaterialPageRoute(
-                          builder: (context) => PrincipalPage(),
-                        );
+                        await _googleSignIn.signIn();
                         setState(() {});
-                        Navigator.pushReplacement(context, route);
                       },
                 child: Text("Iniciar Sesion")),
+            ElevatedButton(
+                onPressed: user == null
+                    ? null
+                    : () async {
+                        await _googleSignIn.signOut();
+                        setState(() {});
+                      },
+                child: Text("Cerrar Sesion")),
           ],
         ),
       ),
