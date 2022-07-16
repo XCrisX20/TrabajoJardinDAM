d4745b83579315d2b53df7a6b1dd193e0dd79b10
Emanuel Venegas <emanuelvperez2000@gmail.com>
2022-07-15 20:32:09 -0400

    Login funcional

--- jardin_cliente/lib/PagesPrincipal/login.dart
+++ jardin_cliente/lib/PagesPrincipal/login.dart
@@ -1,18 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
-class Login_Principal extends StatelessWidget {
-  const Login_Principal({Key? key}) : super(key: key);
+class IniciarSesion extends StatefulWidget {
+  const IniciarSesion({Key? key}) : super(key: key);
+
+  @override
+  State<IniciarSesion> createState() => _IniciarSesionState();
+}
+
+class _IniciarSesionState extends State<IniciarSesion> {
+  GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
 
   @override
   Widget build(BuildContext context) {
+    GoogleSignInAccount? user = _googleSignIn.currentUser;
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hola Usuario'),
+        title: Text('Hola Usuario ' + (user == null ? 'entrada' : 'salida')),
         leading: Icon(MdiIcons.humanHandsup),
       ),
       body: Center(
-        child: ElevatedButton(onPressed: () {}, child: Text("")),
+        child: Column(
+          children: [
+            ElevatedButton(
+                onPressed: user != null
+                    ? null
+                    : () async {
+                        await _googleSignIn.signIn();
+                        setState(() {});
+                      },
+                child: Text("Iniciar Sesion")),
+            ElevatedButton(
+                onPressed: user == null
+                    ? null
+                    : () async {
+                        await _googleSignIn.signOut();
+                        setState(() {});
+                      },
+                child: Text("Cerrar Sesion")),
+          ],
+        ),
       ),
     );
   }
